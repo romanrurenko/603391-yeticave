72be745f712a889904712df62c08fe1a5fa9c9d3
romanrurenko <rroman7@yandex.ru>
2018-11-23 11:52:53 +0700

    modified:   data.php 	modified:   functions.php 	modified:   index.php 	modified:   templates/index.php 	modified:   templates/layout.php

--- data.php
+++ data.php
@@ -1,7 +1,43 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: RTR
- * Date: 22.11.2018
- * Time: 16:13
- */
+$is_auth = rand(0, 1);
+$user_name = 'Test user';
+$user_avatar = 'img/user.jpg';
+$categories = ["Доски и лыжи", "Крепления", "Ботинки", "Одежда", "Инструменты", "Разное"];
+$ads = [
+    0 => [
+        'title' => '2014 Rossignol District Snowboard',
+        'category' => 0,
+        'price' => 10999,
+        'url' => 'img/lot-1.jpg'
+    ],
+    1 => [
+        'title' => 'DC Ply Mens 2016/2017 Snowboard',
+        'category' => 0,
+        'price' => 159999,
+        'url' => 'img/lot-2.jpg'
+    ],
+    2 => [
+        'title' => 'Крепления Union Contact Pro 2015 года размер L/XL',
+        'category' => 1,
+        'price' => 8000,
+        'url' => 'img/lot-3.jpg'
+    ],
+    3 => [
+        'title' => 'Ботинки для сноуборда DC Mutiny Charocal',
+        'category' => 2,
+        'price' => 10999,
+        'url' => 'img/lot-4.jpg'
+    ],
+    4 => [
+        'title' => 'Куртка для сноуборда DC Mutiny Charocal',
+        'category' => 3,
+        'price' => 7500,
+        'url' => 'img/lot-5.jpg'
+    ],
+    5 => [
+        'title' => 'Маска Oakley Canopy',
+        'category' => 5,
+        'price' => 5400,
+        'url' => 'img/lot-6.jpg'
+    ]
+];
--- functions.php
+++ functions.php
@@ -1,7 +1,33 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: RTR
- * Date: 22.11.2018
- * Time: 16:10
- */
+function include_template($name, $data)
+{
+    $name = 'templates/' . $name;
+    $result = '';
+
+    if (!file_exists($name)) {
+        return $result;
+    }
+
+    ob_start();
+    extract($data);
+    require $name;
+
+    $result = ob_get_clean();
+
+    return $result;
+}
+
+function format_price($value)
+{
+    $value = ceil($value);
+    if ($value > 1000) {
+        $value = number_format($value, 0, '', ' ');
+    }
+    return $value . ' <b class="rub">р</b>';
+}
+
+function esc($str)
+{
+    $text = htmlspecialchars($str);
+    return $text;
+}
