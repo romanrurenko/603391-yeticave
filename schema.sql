4d594f2670bd51c8bc3e3b469e5b44344cff2dd8
romanrurenko <rroman7@yandex.ru>
2018-11-26 01:01:18 +0700

    Added table to DB

--- schema.sql
+++ schema.sql
@@ -4,6 +4,15 @@ default collate utf8_general_ci;
 
 USE `yeticave`;
 
+CREATE TABLE IF NOT EXISTS `bids` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `start_date` int(11) unsigned NOT NULL,
+  `user_id` int(11) unsigned NOT NULL,
+  `lot_id` int(11) unsigned NOT NULL,
+  `amount` int(11) unsigned NOT NULL,
+  PRIMARY KEY (`id`)
+  ) ENGINE=InnoDB;
+
 CREATE TABLE IF NOT EXISTS `categories` (
   `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
   `name` char(255) NOT NULL,
@@ -11,15 +20,18 @@ CREATE TABLE IF NOT EXISTS `categories` (
   UNIQUE KEY `name` (`name`)
   ) ENGINE=InnoDB;
 
-
 CREATE TABLE IF NOT EXISTS `lots` (
   `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
-  `user_id` int(11) NOT NULL,
-  `category_id` int(11) NOT NULL,
+  `date_add` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `title` char(255) NOT NULL,
-  `price` int(11) NOT NULL,
+  `description` text NOT NULL,
   `image_url` varchar(100) NOT NULL,
-  `dt_add` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `start_price` int(11) unsigned NOT NULL,
+  `date_end` timestamp NOT NULL,
+  `bit_step` int(11) unsigned NOT NULL,
+  `owner_id` int(11) unsigned NOT NULL,
+  `winner_id` int(11) unsigned DEFAULT NULL,
+  `category_id` int(11) unsigned NOT NULL,
   PRIMARY KEY (`id`)
   ) ENGINE=InnoDB;
 
@@ -28,7 +40,9 @@ CREATE TABLE IF NOT EXISTS `users` (
   `email` char(128) NOT NULL,
   `password` char(64) NOT NULL,
   `name` char(128) NOT NULL,
-  `dt_add` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `avatar_url` char(128) DEFAULT NULL,
+  `contacts` varchar(512) NOT NULL,
+  `date_add` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
   PRIMARY KEY (`id`),
   UNIQUE KEY `email` (`email`),
   UNIQUE KEY `name` (`name`)
